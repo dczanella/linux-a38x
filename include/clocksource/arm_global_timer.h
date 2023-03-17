5bb81bd46e5e8679591b8a6ff058d46eb3127d05
Danilo Cezar Zanella <danilo.zanella@iag.usp.br>
2023-03-17 10:55:49 +0000

    Backport for compatibility with ALMA kernel modules. Specifics for LLAMA uABM         - In recent kernel kallsyms is not exported anymore, but still required by ALMA modules         - Exported ARM global counter to replace x86 rdtsc in ALMA teHandler module

---
+++ include/clocksource/arm_global_timer.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2012 ARM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __CLKSOURCE_ARM_GLOBAL_TIMER_H
+#define __CLKSOURCE_ARM_GLOBAL_TIMER_H
+
+#include <linux/types.h>
+
+#ifdef CONFIG_ARM_GLOBAL_TIMER
+
+u64 gt_counter_read(void);
+
+#endif
+
+#endif
